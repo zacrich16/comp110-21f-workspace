e4a956b4f681a539a7fe6d00cb9ef027179d978c
Zachary Richardson <zachary.richardson@unc.edu>
2021-09-19 16:23:17 -0400

    Updated full project, need to fix 3 errors

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -9,21 +9,14 @@ from random import randint
 points: int = 0
 player: str
 
-# define greet function and welcome message
+
 def greet() -> None:
     """Welcome message and player name."""
     print("Welcome to the UNC Football Touchdown Challenge!")
     print("You pick the play and see if you can score against the CPU!")
     global player
     player = input("Enter Player Name: ")
-
-def op_1() -> None:
-
-
-# define main function
-def main() -> None:
-    """The program's entrypoint."""
-    greet()
+    return None
 
 
 def one_play_touchdown() -> None:
@@ -42,6 +35,8 @@ def one_play_touchdown() -> None:
     again: int = int(input("Play again? Choose yes (0) or no (1)")
     if again == 0:
         one_play_touchdown()
+    else:
+        return None
 
 # pick play
 # make run = 1 and pass = 2
@@ -50,6 +45,84 @@ def one_play_touchdown() -> None:
 # if play != defense, print TOUCHDOWN
 # print points
 # ask to play again
+
+
+def field_goal_kick(x: int) -> int:
+    """Secondary field goal kicking game mode."""
+    print(f"Welcome, {player}, to the field goal kicking challenge!")
+    print("Choose to kick a field goal between 17-59 yards.")
+    print("Longer kicks are harder to make, but see how good of a kicker you are!")
+    dist: int = int(input("Choose your field goal distance (17-59): "))
+    if dist < 30:
+        if dist < 17:
+            return None
+        else:
+            prob: int = randint(1, 2)
+            if prob == 1:
+                print("It's Good!")
+                points += 3
+                print(f"Score: {player} - {points} \n CPU: 0")
+            else:
+                print("No Good")
+    else:
+        if dist < 50:
+            if dist < 40:
+                prob: int = randint(1, 3)
+                if prob == 1:
+                    print("It's Good")
+                    points += 3
+                    print(f"Score: {player} - {points} \n CPU: 0")
+                else:
+                    print("No Good")
+            else:
+                prob: int = randint(1, 4)
+                if prob == 1:
+                    print("It's Good")
+                    points += 3
+                    print(f"Score: {player} - {points} \n CPU: 0")
+                else:
+                    print("No Good")
+        else:
+            prob: int = randint(1, 5)
+                if prob == 1:
+                    print("It's Good")
+                    points += 3
+                    print(f"Score: {player} - {points} \n CPU: 0")
+                else:
+                    print("No Good")
+    again: int = int(input("Play again? Choose Yes (0) or No (1)")
+    if again == 0:
+        field_goal_kick(points)
+    else:
+        return points
+
+# print welcome message
+# pick distance
+# randint make or miss
+# if randint == 1, print It's Good
+# if randint != 1, print No Good
+
+
+def end_game() -> None:
+    """The end of the game."""
+    done: int = int(input("Are you sure you want to quit? Choose Yes (0) or No (1) "))
+    if done == 0:
+        print(f"Goodbye, {player}, you played well today!")
+        print(f"Score: {player} - {points} \n CPU: 0")
+        return None
+    else:
+        return None
+
+
+# define main function
+def main() -> None:
+    """The program's entrypoint."""
+    greet()
+    mode: int = int(input("Choose your game mode: One Play Touchdown (1) or Field Goal Kicker (2) "))
+    if mode == 1:
+        one_play_touchdown()
+    else:
+        field_goal_kick(points)
 
 # 3 possible paths:
 # 1) custom procedure, ask for input, reassign points global var, 1 pt per choice
