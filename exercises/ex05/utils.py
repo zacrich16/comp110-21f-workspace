9bb4b17dd7431c7786c105a15a15c5deb8259ef7
Zachary Richardson <zachary.richardson@unc.edu>
2021-10-04 18:54:35 -0400

    Finished ex05 and unit tests

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,42 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730408740"
 
 # Define your functions below
+
+
+def only_evens(a: list[int]) -> list[int]:
+    i = 0
+    evens: list[int] = []
+    while i < len(a):
+        if a[i] % 2 == 0:
+            evens.append(a[i])
+        i += 1
+    return evens
+
+
+def sub(a: list[int], i: int, j: int) -> list[int]:
+    sub_list: list[int] = []
+    if i < 0:
+        i = 0
+    if j > len(a):
+        j = len(a)
+    if len(a) == 0 or i > len(a) or j <= 0:
+        return sub_list
+    while i < j:
+        sub_list.append(a[i])
+        i += 1
+    return sub_list
+
+
+def concat(a: list[int], b: list[int]) -> list[int]:
+    a_plus_b: list[int] = []
+    i = 0
+    while i < len(a):
+        a_plus_b.append(a[i])
+        i += 1
+    i = 0
+    while i < len(b):
+        a_plus_b.append(b[i])
+        i += 1
+    return a_plus_b
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,77 @@
 """Unit tests for list utility functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730408740"
+
+
+def test_only_evens_empty() -> None:
+    """Test only_evens with empty list."""
+    a: list[int] = []
+    assert only_evens(a) == []
+
+
+def test_only_evens_one_item() -> None:
+    """Test only_evens with one even and odd item."""
+    a: list[int] = [1, 2]
+    assert only_evens(a) == [2]
+
+
+def test_only_evens_multiple_items() -> None:
+    """Test only_evens with multiple even and odd items."""
+    a: list[int] = [1, 2, 3, 4]
+    assert only_evens(a) == [2, 4]
+
+
+def test_sub_empty() -> None:
+    """Test sub with empty list."""
+    a: list[int] = []
+    i: int = 1
+    j: int = 3
+    assert sub(a, i, j) == []
+
+
+def test_sub_i_less_0() -> None:
+    """Test sub if i < 0."""
+    a: list[int] = [1, 2, 3, 4]
+    i: int = -1
+    j: int = 3
+    assert sub(a, i, j) == [1, 2, 3]
+
+
+def test_sub_j_greater_len() -> None:
+    """Test sub if j > length of the list."""
+    a: list[int] = [1, 2, 3, 4]
+    i: int = 1
+    j: int = 5
+    assert sub(a, i, j) == [2, 3, 4]
+
+
+def test_sub_normal_list() -> None:
+    """Test sub if j !> length of the list, i !< 0, and the list is not empty."""
+    a: list[int] = [1, 2, 3, 4]
+    i: int = 1
+    j: int = 3
+    assert sub(a, i, j) == [2, 3]
+
+
+def test_concat_empty_lists() -> None:
+    """Test concat if both lists are empty."""
+    a: list[int] = []
+    b: list[int] = []
+    assert concat(a, b) == []
+
+
+def test_concat_one_empty() -> None:
+    """Test concat if one list is empty."""
+    a: list[int] = [1, 2, 3]
+    b: list[int] = []
+    assert concat(a, b) == [1, 2, 3]
+
+
+def test_concat_two_lists() -> None:
+    """Test concat if both lists have multiple values."""
+    a: list[int] = [1, 2, 3]
+    b: list[int] = [4, 5, 6]
+    assert concat(a, b) == [1, 2, 3, 4, 5, 6]
