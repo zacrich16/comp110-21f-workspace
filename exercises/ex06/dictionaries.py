584b14db507583e13f7797431df070191aae82c0
Zachary Richardson <zachary.richardson@unc.edu>
2021-10-20 01:30:40 -0400

    updated ex06 with docstrings

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -4,6 +4,7 @@ __author__ = "730408740"
 
 
 def invert(xs: dict[str, str]) -> dict[str, str]:
+    """Switch a dictionary's keys and values."""
     result: dict[str, str] = {}
     for key, value in xs.items():
         if value in result:
@@ -13,6 +14,7 @@ def invert(xs: dict[str, str]) -> dict[str, str]:
 
 
 def favorite_color(colors: dict[str, str]) -> str:
+    """Calculate the most frequently stated value in a dictionary."""
     numeric_dict: dict[str, int] = {}
     favorite: str = ""
     if colors == {}:
@@ -27,6 +29,7 @@ def favorite_color(colors: dict[str, str]) -> str:
 
 
 def count(items: list[str]) -> dict[str, int]:
+    """Count the number of times a value appears in a list and assign it to a key-pair value."""
     result: dict[str, int] = {}
     for item in items:
         if item in result:
